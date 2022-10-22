f7270308a04b59ec472bb920c5bb3c59c3d57c4b
tjpc3 <tjpc3@users.noreply.github.com>
2022-10-22 00:56:58 -0400

    working on forms

--- dist/app.js
+++ dist/app.js
@@ -5,14 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const estimator_1 = require("./cost_estimation/estimator");
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/estimate', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname + '/../res/estimate.html'));
+});
+app.post('/estimate_result', (req, res) => {
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+    res.send((0, estimator_1.estimateCost)(40.448819, -79.953920, estimator_1.Medication.SPIRO));
 });
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
-app.use(express_1.default.static(`res`));
-console.log((0, estimator_1.estimateCost)(40.448819, -79.953920, estimator_1.Medication.SPIRO));
+app.use(express_1.default.static(`static`));
+console.log();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import {estimateCost, Medication} from './cost_estimation/estimator';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/estimate', (req, res) => {
+    res.sendFile(path.join(__dirname+'/../res/estimate.html'));
+});
+
+app.post('/estimate_result', (req, res) => {
+    let latitude = req.body.latitude;
+    let longitude = req.body.longitude;
+
+    
+
+    res.send(estimateCost(40.448819, -79.953920, Medication.SPIRO));
 });
 
 app.listen(port, () => {
@@ -14,4 +24,4 @@ app.listen(port, () => {
 
 app.use(express.static(`static`));
 
-console.log(estimateCost(40.448819, -79.953920, Medication.SPIRO));
+console.log();
