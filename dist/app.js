278d0ea2d02ae27e6e5f283546ad26ea8b7ba085
tjpc3 <tjpc3@users.noreply.github.com>
2022-10-22 10:19:40 -0400

    geocoding works!

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const estimator_1 = require("./cost_estimation/estimator");
 const body_parser_1 = __importDefault(require("body-parser"));
+const node_geocoder_1 = __importDefault(require("node-geocoder"));
+const geocoderOptions = {
+    provider: 'google',
+    apiKey: 'AIzaSyA0caYqmHfBYnEERq4GzumQSmFeU88YB6E'
+};
+const geocoder = (0, node_geocoder_1.default)(geocoderOptions);
 const app = (0, express_1.default)();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.use(body_parser_1.default.urlencoded({ extended: false }));
-app.get('/estimate', (req, res) => {
-    //console.log(req.query);
-    if (req.query.latitude && req.query.longitude) {
-        res.send(`test test`);
+app.get('/estimate', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (req.query.address) {
+        const coords = yield geocoder.geocode(req.query.address);
+        res.send((0, estimator_1.estimateCost)(coords[0].latitude, coords[0].longitude, estimator_1.Medication.ESTRADIOL_INJECTIONS));
     }
     else {
         res.render(`estimate`);
-        //res.sendFile(path.join(__dirname+'/../res/estimate.html'));
     }
-});
+}));
 app.listen(port, () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
 app.use(express_1.default.static(`static`));
-console.log();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,13 @@ import express from 'express';
 import {estimateCost, Medication} from './cost_estimation/estimator';
 import path from 'path';
 import bodyParser from 'body-parser'
+import NodeGeocoder from 'node-geocoder';
 
+const geocoderOptions = {
+    provider: 'google',
+    apiKey: 'AIzaSyA0caYqmHfBYnEERq4GzumQSmFeU88YB6E'
+  };
+const geocoder = NodeGeocoder(geocoderOptions);
 const app = express();
 const port = 3000;
 
@@ -10,13 +16,13 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/estimate', (req, res) => {
-    //console.log(req.query);
-    if (req.query.latitude && req.query.longitude) {
-        res.send(`test test`);
+app.get('/estimate', async (req, res) => {
+    if (req.query.address) {
+        const coords = await geocoder.geocode(req.query.address);
+        
+        res.send(estimateCost(coords[0].latitude, coords[0].longitude, Medication.ESTRADIOL_INJECTIONS));
     } else {
         res.render(`estimate`);
-        //res.sendFile(path.join(__dirname+'/../res/estimate.html'));
     }
 
 });
@@ -26,5 +32,3 @@ app.listen(port, () => {
 });
 
 app.use(express.static(`static`));
-
-console.log();
